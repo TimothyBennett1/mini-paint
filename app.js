ed6864a1db4b74077c40488c91480eb023753796
TimothyBennett1 <timothybennett2323@gmail.com>
2016-09-01 13:46:17 -0500

    commiting todays lecture work

---
+++ app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+
+  var currentColor = "white";
+
+
+  $('.box').click(function() {
+    $(this).addClass(currentColor);
+  })
+  // $('.box').on("dblclick", function() {
+  //   $(this).removeClass(currentColor);
+  // });
+
+  $("#reset").click(function() {
+    $(".box").removeClass('red blue green yellow white')
+  });
+
+  $(".color-item").click(function() {
+    currentColor = $(this).text().toLowerCase();
+  })
+
+
+});
